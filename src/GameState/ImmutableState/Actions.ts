09294ce83fa8e679937349b10abca075f8f23be3
LeanKhan <eslean20@gmail.com>
2019-06-25 03:16:29 +0100

    Create Actions.ts to store match actions

---
+++ src/GameState/ImmutableState/Actions.ts
@@ -0,0 +1,15 @@
+const Actions = {
+  general: {
+    pass: {
+      short: 1,
+      medium: 2,
+      long: 3,
+    },
+    shoot: {
+      curved: {
+        diagonal: true,
+      },
+    },
+    move: {},
+  },
+};
