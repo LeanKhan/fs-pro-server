0a3ff7bf394d6a38a30f816ab486626ede0aa74b
LeanKhan <eslean20@gmail.com>
2020-01-21 22:11:56 +0100

    feat: remove excess 'express' imports

--- src/routers/player.router.ts
+++ src/routers/player.router.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import { Router } from 'express';
 import respond from '../helpers/responseHandler';
 import { fetchAllPlayers, createNewPlayer } from '../services/player.service';
 import { getCurrentCounter } from '../middleware/player';
 import { incrementCounter } from '../utils/counter';
 
-const router = express.Router();
+const router = Router();
 
 /**
  * Fetch all players
